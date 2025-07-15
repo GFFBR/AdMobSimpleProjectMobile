c5c54803507dcfc810315a5ac79e408f78d44f74
Gaffber <sdarky777@gmail.com>
2025-07-15 09:19:40 -0300

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+// build.gradle.kts (Nível do Projeto - VERSÃO FINAL)
+// A configuração de repositórios foi movida para settings.gradle.kts
+
+plugins {
+    id("com.android.application") version "8.4.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+}
